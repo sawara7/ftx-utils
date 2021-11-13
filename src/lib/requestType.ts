0db5374da4edbdcffeea7a7ff8700250ab60ecc8
sawara7 <sawara7@gmail.com>
2021-11-13 14:45:25 +0900

    Modified HistoricalPricesRequest.

--- src/lib/requestType.ts
+++ src/lib/requestType.ts
@@ -22,9 +22,8 @@ export interface GetTradesRequest {
 export interface GetHistoricalPricesRequest {
     market_name: string	//BTC-0628	name of the market
     resolution: number //300	window length in seconds. options: 15, 60, 300, 900, 3600, 14400, 86400
-    limit: number //35	optional, max 5000
-    start_time: number //1559881511	optional
-    end_time: number //1559881711	optional
+    start_time?: number //1559881511	optional
+    end_time?: number //1559881711	optional
 }
 
 export interface GetFundingRateRequest {
