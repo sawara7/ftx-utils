f154dd9235b86c9385675d2d387775f2653a7f37
sawara7 <sawara7@gmail.com>
2022-01-23 13:43:55 +0900

    Fix.

--- dist/lib/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-function sleep(waitSec) {
-    return new Promise(function (resolve) {
-        setTimeout(function () { resolve(); }, waitSec);
-    });
-}
-exports.sleep = sleep;
